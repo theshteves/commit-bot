aab144016edc4bbf74523de4b09c855cb3815463
theshteves <kneisers@msu.edu>
2014-12-17 23:55:46 -0500

    Bot is now live

--- bot.sh
+++ bot.sh
@@ -1,15 +1,17 @@
+# crontab: 0 22 * * * source ~/Desktop/code/commit-bot/bot.sh
+
 cd ~/Desktop/code/commit-bot
 clear
 
-#Create temporary file with new line in place
+###Create temporary file with new line in place
 cat output.txt > temp_file.txt
 echo Commit: $(date) >> temp_file.txt
 
-#Copy the new file over the original file
-mv temp_file.txt output.txt
-
 echo Commit: $(date)
 echo
+
+###Copy the new file over the original file
+mv temp_file.txt output.txt
 
 # git remote set-url origin git@github.com:theshteves/commit-bot.git
 
