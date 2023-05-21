0202c59d8ab073080d04de7a9c22c007cb91470e
gsspdev <108854023+gsspdev@users.noreply.github.com>
2023-05-21 03:42:23 +0000

    removed some stuff

--- bot.sh
+++ bot.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 #
-# Commit Bot by Steven Kneiser
-#
-# > https://github.com/theshteves/commit-bot
-#
 # Deploy locally by adding the following line to your crontab:
 # 0 22 * * * /bin/bash /<full-path-to-your-folder>/code/commit-bot/bot.sh
 #
 # Edit your crontab in vim w/ the simple command:
 # crontab -e
 #
-# Deploying just on your computer is better than a server if you want
-# your commits to more realistically mirror your computer usage.
 #
-# ...c'mon, nobody commits EVERY day ;)
-#
+
 info="Commit: $(date)"
 os="$(uname -s)"
 
